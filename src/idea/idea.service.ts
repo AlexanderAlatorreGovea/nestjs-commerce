9f2f70672a9e7f8a273d3dde226bfdf39f04c80e
Alexander <Alexander_alatorre1993@hotmail.com>
2022-01-25 17:36:44 -0800

    added services

---
+++ src/idea/idea.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { IdeaEntity } from './idea.entity';
+
+@Injectable()
+export class IdeaService {
+  constructor(
+    @InjectRepository(IdeaEntity)
+    private ideaRepository: Repository<IdeaEntity>,
+  ) {}
+}
