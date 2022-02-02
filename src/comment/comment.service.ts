019e59d2404bd7283354e767c1233013873e0801
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-01 18:31:46 -0800

    added service

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IdeaEntity } from 'idea/idea.entity';
 import { Repository } from 'typeorm';
@@ -24,5 +24,56 @@ export class CommentService {
     });
   }
 
-  async create(ideaId: string, userId: string, data: CommentDTO) {}
+  async create(ideaId: string, userId: string, data: CommentDTO) {
+    const idea = await this.ideaRepository.findOne({ where: { id: ideaId } });
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+
+    const comment = await this.commentRepository.create({
+      ...data,
+      idea,
+      author: user,
+    });
+
+    await this.commentRepository.save(comment);
+
+    return comment;
+  }
+
+  async destroy(id: string, userId: string) {
+    const comment = await this.commentRepository.findOne({
+      where: { id },
+      relations: ['author', 'idea'],
+    });
+
+    if (comment.author.id !== userId) {
+      throw new HttpException(
+        'You do not own this comment',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    await this.commentRepository.remove(comment);
+
+    return comment;
+  }
+
+  async showByIdea(id: string) {
+    const idea = await this.ideaRepository.findOne({
+      where: { id },
+      relations: ['comments', 'comments.author', 'comments.idea'],
+    });
+
+    return idea.comments;
+  }
+
+  async showByUser(id: string) {
+    const comments = await this.commentRepository.find({
+      where: {
+        author: { id },
+      },
+      relations: ['author'],
+    });
+
+    return comments;
+  }
 }
