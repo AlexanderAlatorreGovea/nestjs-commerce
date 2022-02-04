39ba19775c3f9ca3dc4594cc23483e26e52c6fab
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-03 20:02:03 -0800

    added graphql

--- src/shared/logging.interceptor.ts
+++ src/shared/logging.interceptor.ts
@@ -1,10 +1,11 @@
 import {
-  Injectable,
-  NestInterceptor,
-  ExecutionContext,
   CallHandler,
+  ExecutionContext,
+  Injectable,
   Logger,
+  NestInterceptor,
 } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { Observable, tap } from 'rxjs';
 
 const getLoggerMessage = ({ method, url, now, context }) => {
@@ -18,6 +19,24 @@ const getLoggerMessage = ({ method, url, now, context }) => {
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     const req = context.switchToHttp().getRequest();
+
+    if (!req) {
+      const ctx: any = GqlExecutionContext.create(context);
+      const resolverName = ctx.constructorRef.name;
+      const info = ctx.getInfo();
+
+      return handler
+        .handle()
+        .pipe(
+          tap(() =>
+            Logger.log(
+              `${info.parentType} "${info.fieldName}" ${Date.now() - now}ms`,
+              resolverName,
+            ),
+          ),
+        );
+    }
+
     const method = req.method;
     const url = req.url;
     const now = Date.now();
---
+++ src/user/user.resolver.ts
@@ -0,0 +1,14 @@
+import { Query, Resolver } from '@nestjs/graphql';
+
+@Resolver('User')
+export class UserResolver {
+  @Query()
+  users() {
+    return [
+      {
+        id: 'id',
+        username: 'username',
+      },
+    ];
+  }
+}
